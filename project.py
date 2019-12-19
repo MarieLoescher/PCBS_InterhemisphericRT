0bbccd9d0309a57a76550f7a82e698cd732f279c
MarieLoescher <55835342+MarieLoescher@users.noreply.github.com>
2019-12-19 16:29:01 +0100

    Inserted the code for the trials for each block

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 import pygame.freetype
 
 
@@ -9,6 +9,7 @@ N_BLOCKS = 4
 N_TRIALS = 15
 OUTPUT_FILE = 'reaction_times.csv'
 
+reaction_times = []
 
 def main():
     screen = open_window()
@@ -57,24 +58,35 @@ def get_input():
 
 def run_block(screen, block_number):
     show_instructions(f"block_{block_number}_instructions.txt", screen)
-
+    if block_number in [0, 1]:
+        key = pygame.K_j
+    elif block_number in [2, 3]:
+        key = pygame.K_f
     for trial in range(N_TRIALS):
-        run_trial(screen)
+        run_trial(screen, key)
 
 
-def run_trial(screen):
-    screen.fill(BLACK)
-    r = screen.get_rect()
-    pygame.draw.circle(screen, WHITE, (r.width // 2, r.height // 2), 10)
-    pygame.display.flip()
-    get_input()
+def run_trial(screen, key):
+    while True:
+        screen.fill(BLACK)
+        r = screen.get_rect()
+
+        waiting_time = random.randint(1000, 3000)
+        pygame.display.update()
+        pygame.time.delay(waiting_time)
+
+        pygame.draw.circle(screen, WHITE, (r.width // 2, r.height // 2), 10)
+        pygame.display.flip()
+        t0 = pygame.time.get_ticks()
+
+        if get_input() == key:
+            reaction_times.append(pygame.time.get_ticks() - t0)
+            return
 
 
 def close_window():
     pass
 
-
-#    reaction_times = []
 
 if __name__ == '__main__':
     main()
